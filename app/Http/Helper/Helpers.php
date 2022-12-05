2731b90bffe21e6b02e831470417c2e742af57a6
benyaminrmb <benyaminrmb@gmail.com>
2022-12-05 06:48:13 +0330

    generate helper added helper_to_array method for build an array of resource

---
+++ app/Http/Helper/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('helper_to_array')) {
+    /**
+     * Create array of (Resource Collection)
+     * @param $data
+     * @return array|int
+     */
+    function helper_to_array($data): array|int
+    {
+        if (is_object($data)) {
+            $data = json_decode(Str::replace('.','',$data->toJson()), true);
+        }
+        return $data;
+    }
+}
